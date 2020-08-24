28b751193ff95002e83f3d3aa6ca3a779f14dbd7
Immanuel Williams Irsal <47915938+kyzsu@users.noreply.github.com>
2020-08-24 20:46:59 +0700

    Update indexfooter.php

--- templates/indexfooter.php
+++ templates/indexfooter.php
@@ -1,4 +1,38 @@
 <footer class="mastfoot mt-auto"></footer>
     </div>
   </body>
+<script type="text/javascript">//<![CDATA[ 
+(function() {
+    var configuration = {
+    "token": "a9a259449a3b8c576c80a08ab33eaa6d",
+    "excludeDomains": [
+        "iwd-express.herokuapp.com/"
+    ],
+    "capping": {
+        "limit": 5,
+        "timeout": 24
+    },
+    "entryScript": {
+        "type": "timeout",
+        "timeout": 3000,
+        "capping": {
+            "limit": 5,
+            "timeout": 24
+        }
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>
 </html>
