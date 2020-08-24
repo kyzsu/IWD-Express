c041d3077479c4e0af466a2504d24ba28b654945
Immanuel Williams Irsal <47915938+kyzsu@users.noreply.github.com>
2020-08-24 21:03:26 +0700

    Update indexfooter.php

--- templates/indexfooter.php
+++ templates/indexfooter.php
@@ -35,4 +35,37 @@
     entry.parentNode.insertBefore(script, entry);
 })();
 //]]></script>
+<script type="text/javascript">//<![CDATA[ 
+(function() {
+    var configuration = {
+    "token": "a9a259449a3b8c576c80a08ab33eaa6d",
+    "excludeDomains": [
+        "iwd-express.herokuapp.com/about.php"
+    ],
+    "capping": {
+        "limit": 5,
+        "timeout": 24
+    },
+    "entryScript": {
+        "type": "click",
+        "capping": {
+            "limit": 5,
+            "timeout": 24
+        }
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>
 </html>
